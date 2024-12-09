81bc3895c2f7664d3d73fb316f1ef9665e81697d
HeitorNOC <heiitornoc@gmail.com>
2024-12-09 11:20:49 -0300

    backend connection

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -6,7 +6,6 @@ export function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
-      <h1>AppLayout</h1>
 
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
